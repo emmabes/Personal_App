6fd8617c817dd1510679c8cd86d308e1e1bca0a9
Erik Mabes <eam233@gmail.com>
2025-08-29 11:37:35 -0700

    initialized frontend (react via vite); adding frontend pipeline

---
+++ PersonalAppFront/PersonalAppFrontCDK/bin/personal_app_front_cdk.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { PersonalAppFrontCdkStack } from '../lib/personal_app_front_cdk-stack';
+import { FrontendPipelineStack } from './pipeline';
+
+const app = new cdk.App();
+const ENVIRONMENT = {'account': app.node.tryGetContext('AWS_ACCOUNT'), 'region': app.node.tryGetContext('AWS_REGION')};
+if (!ENVIRONMENT['account'] || !ENVIRONMENT['region']) {
+    throw new Error(`CDK context ${
+        !ENVIRONMENT['account'] && !ENVIRONMENT['region'] ? 'account and region' :
+        !ENVIRONMENT['account'] ? 'account' :
+        !ENVIRONMENT['region'] ? 'region' : ''
+    } were not captured during initialization.`)
+}
+
+new FrontendPipelineStack(app, 'PersonalAppFrontendPipelineStack', {
+    env: ENVIRONMENT
+});
+
+new PersonalAppFrontCdkStack(app, 'PersonalAppFrontCdkStack', {
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+    * Account/Region-dependent features and context lookups will not work,
+    * but a single synthesized template can be deployed anywhere. */
+
+    /* Uncomment the next line to specialize this stack for the AWS Account
+    * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+    /* Uncomment the next line if you know exactly what Account and Region you
+    * want to deploy the stack to. */
+    // env: { account: '123456789012', region: 'us-east-1' },
+
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+
+    env: ENVIRONMENT,
+});
---
+++ PersonalAppFront/PersonalAppFrontCDK/bin/pipeline.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from 'aws-cdk-lib/pipelines'; 
+import { Construct } from 'constructs';
+import { PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
+
+export class FrontendPipelineStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const synthStep = new ShellStep('Synth', {
+            input: CodePipelineSource.gitHub('emmabes/personal_app', 'main', {
+                authentication: cdk.SecretValue.secretsManager('personal_app_frontend_pipeline_token'),
+            }), 
+            commands: [
+                'if [ "$(git diff --name-only HEAD^ HEAD | grep ^PersonalAppFrontend/)" ]; then export DO_BUILD=true; else export DO_BUILD=false; fi',
+                'if [ "$DO_BUILD" = "false" ]; then echo "No changes in PersonalAppFrontend/, skipping build."; exit 0; fi',
+                'cd ../frontend',
+                'cd infra', 
+                'npm install',
+                'npx cdk synth',
+            ],
+            primaryOutputDirectory: 'infra/cdk.out',
+        });
+
+        const pipeline = new CodePipeline(this, 'MyFrontendPipeline', {
+            pipelineName: 'FrontendPipeline',
+            synth: synthStep,
+            selfMutation: true,
+            crossAccountKeys: false,
+        });
+
+        const secretsArn = cdk.SecretValue.secretsManager('personal_app_frontend_pipeline_token');
+
+        const synthProject = pipeline.pipeline.role.addToPrincipalPolicy(
+            new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ['sercretsmanager:GetSecretValue'],
+                resources: [
+                    `arn:aws:secretsmanager:${this.region}:${this.account}:secret:personal_app_frontend_pipeline_token`
+                ],
+            })
+        );
+    }
+}
