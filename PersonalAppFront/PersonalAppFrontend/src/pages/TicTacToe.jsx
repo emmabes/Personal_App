8491baeca74bc22d34a39ff03ab7d2a72e264adb
Erik Mabes <eam233@gmail.com>
2025-11-13 00:52:32 -0700

    Correction for failed build

--- PersonalAppFront/PersonalAppFrontend/src/pages/TicTacToe.jsx
+++ PersonalAppFront/PersonalAppFrontend/src/pages/TicTacToe.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./TicTacToe.css";
 import backgroundImage from "../assets/Solid-Black-Website-Background.jpg";
-import { checkWinner, checkCatsGame } from "../services/ttc/gameLogic";
+import { checkWinner, checkCatsGame } from "../services/ttc/gameLogic.js";
 import { iconList, renderIcon } from "../services/ttc/iconService.jsx";
 import GameGrid from "../components/ttc/GameGrid";
 import PlayerBox from "../components/ttc/PlayerBox";
